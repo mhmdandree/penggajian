77829657160e5e47297ebbedeb0825e96de60b05
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2022-05-27 09:11:24 +0700

    ubah label

--- application/views/ganti_password.php
+++ application/views/ganti_password.php
@@ -11,7 +11,7 @@
   		<form method="POST" action="<?php echo base_url('ganti_password/ganti_password_aksi')?>">
   			
   			<div class="form-grup">
-  				<label>Password Baru</label>
+  				<label>New Password</label>
   				<input type="password" name="passBaru" class="form-control">
   				<?php echo form_error('paasBaru', '<div class="text-small text-danger"> </div>')?>
   			</div>
